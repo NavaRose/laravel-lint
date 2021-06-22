4b6d30393ee271b91263b28fe4824b52efc6fade
nguyendotrung38 <nguyendotrung38@gmail.com>
2021-06-22 17:03:15 +0700

    Test check language

--- bin/check_language.sh
+++ bin/check_language.sh
@@ -1,36 +1,37 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 . "$DIR"/.color
+main_lang=en
+language_path=./resources/lang/
+language_list=`ls ./resources/lang`
 checking_language () {
-  language_list=`ls ./resources/lang`
-  echo $language_list
-  exit 1
   error_flag=false
-  dir=`ls ./resources/lang/en`
+  dir=`ls ./resources/lang/$main_lang`
 
-  # Scanning in En language dir...
+  # Scanning in main language dir...
   for entry in $dir
   do
-    EN_FILE=./resources/lang/en/$entry
-    JP_FILE=./resources/lang/ja/$entry
+    MAIN_LANGUAGE_FILE=$language_path$main_lang/$entry
+    "$DIR"/utilities.sh count_file_line "$MAIN_LANGUAGE_FILE"
+    main_lang_file_line=$?
 
-    # Check if file exist in Ja language dir
-    if [ -f "$JP_FILE" ]; then
-      # If exist, then checking line number between two language are same
-      "$DIR"/utilities.sh count_file_line "$JP_FILE"
-      jp_lang_file_line=$?
-      "$DIR"/utilities.sh count_file_line "$EN_FILE"
-      en_lang_file_line=$?
+    for lang in $language_list
+    do
+      file_to_compare=$language_path$lang/$entry
+      if [ ! -f "$language_path$lang/$entry" ]; then
+        echo "${RED}[✗] $entry file is not exist in $lang language${RESET_COLOR}"
+        error_flag=true
+      fi
 
-      if [ $jp_lang_file_line != $en_lang_file_line ]; then
-        echo "${RED}[✗] $entry between two languages not same line${RESET_COLOR}"
+      "$DIR"/utilities.sh count_file_line "$file_to_compare"
+      compare_file_line=$?
+
+      if [ $compare_file_line != $main_lang_file_line ]; then
+        echo "${RED}[✗] $entry between $lang and $main_lang languages not same line${RESET_COLOR}"
         error_flag=true
       else
         echo "${GREEN}[✓] $entry${RESET_COLOR} file are matched."
       fi
-    else
-      echo "${RED}[✗] $entry file is not exist in JA language${RESET_COLOR}"
-      error_flag=true
-    fi
+    done
   done
 
   checking_language_result=$(php -f "$DIR"/git_hook_support.php)
