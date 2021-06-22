01d1103f5fc78b2eff59797f910dbdb68dad1b31
nguyendotrung38 <nguyendotrung38@gmail.com>
2021-06-22 10:56:42 +0700

    - Divined checking script to small script for easily management.

---
+++ bin/check_env.sh
@@ -0,0 +1,44 @@
+checking_env_lines () {
+  count_file_line .env
+  env_file_line=$?
+  count_file_line .env.example
+  env_example_file_line=$?
+
+  if [ "$env_file_line"  != "$env_example_file_line" ]; then
+    echo "${RED}[✗] Not match: You cannot commit this change.${RESET_COLOR}"
+    [ ! $DEBUG_MODE == 'true' ] && exit 1
+  else
+    echo "${GREEN}[✓] Lines number of environment files matched.${RESET_COLOR}"
+  fi
+}
+
+checking_env_variable () {
+  error_flag=false
+  ENV_VARIABLE=$(cat .env | sed 's;=.*;;')
+  . .env.example
+  for var in $ENV_VARIABLE
+  do
+    CHECKING_VAR="${!var=::undefined::}"
+    if [ "$CHECKING_VAR" == "::undefined::" ]; then
+      echo "${RED}[✗] Please define $var variable at .env.example${RESET_COLOR}:" ${!var}
+      error_flag=true
+    fi
+  done
+  if [ $error_flag != false ]; then
+      [ ! $DEBUG_MODE == 'true' ] && exit 1
+  else
+    echo "${GREEN}[✓] Variable consistency between environment files matched.${RESET_COLOR}"
+  fi
+}
+
+checking_using_of_env () {
+  use_env_directly=$(grep -rn $ENV_USING_CHECKING_DIRS -e "env([[:alnum:] ',_]*)")
+  if [ "$use_env_directly" != '' ]; then
+    echo "${RED}[✗] Failed: these following files are using environment variables directly:${RESET_COLOR}"
+    echo "$use_env_directly\n"
+    [ ! $DEBUG_MODE == 'true' ] && exit 1
+  else
+    echo "${GREEN}[✓] Checking completed, no files using environment variables in wrong way.${RESET_COLOR}\n"
+  fi
+}
+"$@"
--- pre-commit.sh
+++ pre-commit.sh
@@ -22,6 +22,8 @@ LOGS_FILE_EXTENSION='.log'
 
 LOG_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 
+PACKAGE_NAME='amv-hub/amv-lint/'
+
 # Define text color
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -47,50 +49,6 @@ fi
 count_file_line () {
     arg1=$1
     return "$(wc -l "$arg1" | awk '{print $1}')"
-}
-
-checking_env_lines () {
-  count_file_line .env
-  env_file_line=$?
-  count_file_line .env.example
-  env_example_file_line=$?
-
-  if [ "$env_file_line"  != "$env_example_file_line" ]; then
-    echo "${RED}[✗] Not match: You cannot commit this change.${RESET_COLOR}"
-    [ ! $DEBUG_MODE == 'true' ] && exit 1
-  else
-    echo "${GREEN}[✓] Lines number of environment files matched.${RESET_COLOR}"
-  fi
-}
-
-checking_env_variable () {
-  error_flag=false
-  ENV_VARIABLE=$(cat .env | sed 's;=.*;;')
-  . .env.example
-  for var in $ENV_VARIABLE
-  do
-    CHECKING_VAR="${!var=::undefined::}"
-    if [ "$CHECKING_VAR" == "::undefined::" ]; then
-      echo "${RED}[✗] Please define $var variable at .env.example${RESET_COLOR}:" ${!var}
-      error_flag=true
-    fi
-  done
-  if [ $error_flag != false ]; then
-      [ ! $DEBUG_MODE == 'true' ] && exit 1
-  else
-    echo "${GREEN}[✓] Variable consistency between environment files matched.${RESET_COLOR}"
-  fi
-}
-
-checking_using_of_env () {
-  use_env_directly=$(grep -rn $ENV_USING_CHECKING_DIRS -e "env([[:alnum:] ',_]*)")
-  if [ "$use_env_directly" != '' ]; then
-    echo "${RED}[✗] Failed: these following files are using environment variables directly:${RESET_COLOR}"
-    echo "$use_env_directly\n"
-    [ ! $DEBUG_MODE == 'true' ] && exit 1
-  else
-    echo "${GREEN}[✓] Checking completed, no files using environment variables in wrong way.${RESET_COLOR}\n"
-  fi
 }
 
 checking_language () {
@@ -166,10 +124,10 @@ checking_javascript () {
 
 lint() {
   echo "${BLUE}- Checking environment variable:${RESET_COLOR}"
-  checking_env_lines
-  checking_env_variable
-  checking_using_of_env
-
+  sh ./vendor/"$PACKAGE_NAME"/bin/check_env/checking_env_lines
+  sh ./vendor/"$PACKAGE_NAME"/bin/check_env/checking_env_variable
+  sh ./vendor/"$PACKAGE_NAME"/bin/check_env/checking_using_of_env
+  exit 1
   # Checking language translation files
   echo "${BLUE}- Checking consistency of language translation files:${RESET_COLOR}"
   checking_language
