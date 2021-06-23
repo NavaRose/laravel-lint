4c22776f0c16a51449fac6dc2ae252f441d90eb9
nguyendotrung38 <nguyendotrung38@gmail.com>
2021-06-23 08:17:28 +0700

    Edit files path

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 
 mv "$DIR"/.amv_lint.env ./amv_lint.env
 mv "$DIR"/.eslintrc.json ./.eslintrc.json
-cp ./vendor/amv-hub/amv-lint/pre-commit.sh /usr/local/bin/amv
+cp "$DIR"/pre-commit.sh /usr/local/bin/amv
 npm i eslint@7.29.0 eslint-plugin-vue@7.11.1 eslint-config-google@0.14.0 babel-eslint @babel/eslint-plugin
 composer require "squizlabs/php_codesniffer=*" --dev
+
 rm -rf "$DIR"/init.sh
